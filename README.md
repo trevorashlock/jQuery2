e09dfd47dc934a1068075e7218a8589bcfb224ce
Trevor Ashlock <trevorashlock@Trevors-MacBook-Pro.local>
2016-02-19 18:31:32 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<img src="https://devmounta.in/img/logowhiteblue.png" width="250" align="right">
+ <img src="https://devmounta.in/img/logowhiteblue.png" width="250" align="right">
 
 Listo with jQuery
 =================
---
+++ app/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+     var listo = [];
+     var Task = function(task) {
+     	this.task = task;
+     	this.id = new;
+     }
+     var addTask = function() {
+        if(task) {
+        	task = new Task(task);
+        	listo.push(task);
+
+        }
+
+     }
+
+
+
+
+
+
+
+
+
+});
